35967f45699ac391eaf68837db0814392079ecdb
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-12 10:46:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class String
 
+# self refers to the string being passed into method since a string is the object for that String class
   def sentence?
     return true if self.end_with?(".")
     return false
