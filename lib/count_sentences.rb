98e3344110a87c1c61bf1527fa51e4e52e54ba4c
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-12 11:02:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,25 @@ require 'pry'
 
 class String
 
+# self refers to the string being passed into method since a string is the object for that String class
   def sentence?
-    
+    return true if self.end_with?(".")
+    return false
   end
 
   def question?
-
+    return true if self.end_with?("?")
+    return false 
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
+    return false 
   end
 
   def count_sentences
-
+    # split using regex after ".", "?", or "!" followed by space
+    a = self.split(/[.!?]\s/)
+    a.count
   end
 end
